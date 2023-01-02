77d99ac5354f8082a501e5102222368cceffea1e
Daniel Chin <daniel.chin@nyu.edu>
2023-01-02 15:25:22 +0400

    i guess i updated some scripts to newer infra

--- n/changeResponse.py
+++ n/changeResponse.py
@@ -3,23 +3,27 @@ Manually change your response to a doc you already
 gave a response to earlier.  
 '''
 
-import ai
-from database import database
+from threading import Lock
+from shared import *
+
+from database import Database
 
 def main():
-    with database:
+    exitLock = Lock()
+    exitLock.acquire()
+    with Database(exitLock) as database:
         doc_id = input('doc_id=')
         doc = database.loadDoc(doc_id)
         print(doc)
         print()
         print('Which responses to change to?')
         print(
-            '', *ai.ALL_RESPONSES, 
+            '', ALL_RESPONSES, 
             sep = '\n  ', 
         )
         print()
         new_response = input('new_response=')
-        assert new_response in ai.ALL_RESPONSES
+        assert new_response in ALL_RESPONSES
         doc.response = new_response
         database.saveDoc(doc)
         database.accOverall(new_response)
--- n/goods.py
+++ n/goods.py
@@ -1,48 +1,55 @@
-from ai import RES_SAVE, RES_WOW
-from database import database
+from os import path
+from threading import Lock
+from typing import List
+from requests import get
+
+from tqdm import tqdm
+
+from shared import *
+from database import Database
 from doc import Doc
-from jdt import Jdt
-from nozo import getImage
-import os.path as osp
+from nozo import IMAGE_HEADERS
+
+def getImage(url: str):
+  response = get(url, headers=IMAGE_HEADERS)
+  return response.content
 
 def main():
-  all_imgs = [x.split('_')[0] for x in database.listAllImgs()]
-  print(len(all_imgs), 'images on disk')
-  all_docs = database.listAllDocs()
-  print(len(all_docs), 'docs viewed')
-  wows = []
-  saves = []
-  with Jdt(len(all_docs), 'filtering', UPP=4) as j:
-    for doc_id in all_docs:
-      j.acc()
+  exitLock = Lock()
+  exitLock.acquire()
+  with Database(exitLock) as database:
+    all_imgs = [x.split('_')[0] for x in database.listAllImgs()]
+    print(len(all_imgs), 'images on disk')
+    all_docs = database.listAllDocs()
+    print(len(all_docs), 'docs viewed')
+    wows: List[Doc] = []
+    saves: List[Doc] = []
+    for doc_id in tqdm(all_docs, 'filtering'):
       doc: Doc = database.loadDoc(doc_id)
       if doc.response == RES_SAVE:
         saves.append(doc)
       elif doc.response == RES_WOW:
         wows.append(doc)
-  lacks = set([x.id for x in saves]) - set(all_imgs)
-  if lacks:
-    print('Document response is SAVE but image not on disk:')
-    [print(x) for x in lacks]
-    print('Download now?')
-    if input('y/n ').lower() == 'y':
-      for id in lacks:
-        doc = [x for x in saves if x.id == id][0]
-        print('Getting', doc.id, '...')
-        imgs = [getImage(x) for x in doc.img_urls]
-        database.saveImg(doc, imgs)
-      print('Complete.')
-  p = input('Input path to download to: ')
-  fns = [(osp.join(p, doc.id) + '.' + doc.img_type, doc) for doc in wows]
-  missing_fns = [x for x in fns if not osp.exists(x[0])]
-  print(f'There are {len(fns)} wow ones. {len(missing_fns)} are not on disk.')
-  with Jdt(len(missing_fns)) as j:
-    for fn, doc in missing_fns:
-      j.acc()
+    lacks = set([x.id for x in saves]) - set(all_imgs)
+    if lacks:
+      print('Document response is SAVE but image not on disk:')
+      [print(x) for x in lacks]
+      print('Download now?')
+      if input('y/n ').lower() == 'y':
+        for id in lacks:
+          doc = [x for x in saves if x.id == id][0]
+          print('Getting', doc.id, '...')
+          imgs = [getImage(x) for x in doc.img_urls]
+          database.saveImg(doc, imgs)
+        print('Complete.')
+    p = input('Input path to download to: ')
+    fns = [(path.join(p, doc.id) + '.' + doc.img_type, doc) for doc in wows]
+    missing_fns = [x for x in fns if not path.exists(x[0])]
+    print(f'There are {len(fns)} wow ones. {len(missing_fns)} are not on disk.')
+    for fn, doc in tqdm(missing_fns):
       img = getImage(doc.img_urls[0])
       with open(fn, 'wb') as f:
         f.write(img)
-  input('Ok. Press enter to quit...')
+    input('Ok. Press enter to quit...')
 
-with database:
-  main()
+main()
